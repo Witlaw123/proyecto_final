7a4b47504a673af977b61778326b00a46e891145
Erick <erick_recio@outlook.com>
2019-07-31 17:19:35 -0600

    Modificaciones en clase miercoles

--- src/proyecto_final/Arbol.java
+++ src/proyecto_final/Arbol.java
@@ -5,10 +5,110 @@
  */
 package proyecto_final;
 
+import java.util.LinkedList;
 /**
  *
  * @author Erick
  */
-public class Arbol {
+public class Arbol { /*ESTA CLASE SIRVE PARA INSERTAR DATOS EN EL ARBOL E
+    Y PREPARARLOS PARA QUE SE ACOMODEN PARA LOS DISTINTOS RECORRIDOS*/
     
+    private Nodo raiz;
+   
+    int alt;    
+    
+    public Arbol(){
+    
+        raiz = null;
+    }
+        
+    public boolean agregar (int dato){
+        Nodo nuevo = new Nodo (dato, null, null);
+        insertar (nuevo, raiz);
+        return true;
+    }
+    
+    /*Con este método vamos a insertar los datos en el arbol de búsqueda*/
+    
+    public void insertar(Nodo nuevo, Nodo pivote){
+        if (this.raiz == null){
+            raiz = nuevo;
+        } else {
+            if (nuevo.getDato() <= pivote.getDato()){
+                if (pivote.getIzq() == null){
+                    pivote.setIzq(nuevo);
+            } else {
+                insertar (nuevo, pivote.getIzq());    
+            }
+        } else {
+            if (pivote.getDer() == null) {
+                pivote.setDer(nuevo);
+            } else {
+                insertar(nuevo, pivote.getDer());
+                }
+            }
+        }
+    }
+    
+    public Nodo getRaiz(){
+        return raiz;
+    }
+    
+    public void setRaiz (Nodo raiz){
+        this.raiz = raiz;
+        
+    }
+    
+   /*MÉTODO PARA EL RECORRIDO PREORDEN, EL MÉTODO RECIBE LA RAIZ Y UNA LISTA
+    ENLAZADA PARA IR ALMACENANDO EL RECORRIDO DE LOS NODOS - NO MODIFICAR
+    POR NINGUN MOTIVO QUE FUE UN ROLLO PARA HACERLO ANDAR*/
+    
+    public LinkedList preOrden() {
+        LinkedList recorrido = new LinkedList();
+        preorden(raiz, recorrido);
+        return recorrido;
+    }
+    
+    public void preorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            recorrido.add(aux.getDato());
+            preorden(aux.getIzq(), recorrido);
+            preorden(aux.getDer(), recorrido);
+        }
+    }
+
+   /*MÉTODO PARA EL RECORRIDO INORDEN, EL MÉTODO RECIBE LA RAIZ Y UNA LISTA
+    ENLAZADA PARA IR ALMACENANDO EL RECORRIDO DE LOS NODOS - NO MODIFICAR POR 
+    NINGUN MOTIVO*/
+    
+    public LinkedList inOrden() {
+        LinkedList recorrido = new LinkedList();
+        inorden(raiz, recorrido);
+        return recorrido;
+    }
+    
+    public void inorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            inorden(aux.getIzq(), recorrido);
+            recorrido.add(aux.getDato());
+            inorden(aux.getDer(), recorrido);
+        }
+    }
+
+    /*MÉTODO PARA EL RECORRIDO POSTORDEN, EL MÉTODO RECIBE LA RAIZ Y UNA LISTA
+    ENLAZADA PARA IR ALMACENANDO EL RECORRIDO DE LOS NODOS - NO MODIFICAR
+    POR NINGUN MOTIVO*/
+    
+    public LinkedList postOrden() {
+        LinkedList recorrido = new LinkedList();
+        postorden(raiz, recorrido);
+        return recorrido;
+    }
+    public void postorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            postorden(aux.getIzq(), recorrido);
+            postorden(aux.getDer(), recorrido);
+            recorrido.add(aux.getDato());
+        }
+    }
 }
---
+++ src/proyecto_final/InsertarArbol.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_final;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Erick
+ */
+public class InsertarArbol {   /*ESTA CLASE SIRVE PARA ENVIAR LOS RECORRIDOS
+    DE LOS DATOS AL TEXT AREA*/
+    
+    Arbol nuevoArbol = new Arbol();
+    
+    public InsertarArbol(){
+    
+    }
+    
+    public boolean insertar(Integer dato) {
+        return (this.nuevoArbol.agregar(dato));
+    }
+    
+    /*ESTOS MÉTODOS SIRVEN PARA MOSTRAR LOS RECORRIDOS POSIBLES DEL ÁRBOL*/
+    
+    public String preOrden() {
+        LinkedList it = this.nuevoArbol.preOrden();
+        return (recorrido(it, "Preorden"));
+    }
+
+    public String inOrden() {
+        LinkedList it = this.nuevoArbol.inOrden();
+        return (recorrido(it,  "Inorden"));
+    }
+
+    public String postOrden() {
+        LinkedList it = this.nuevoArbol.postOrden();
+        return (recorrido(it, "Postorden"));
+    }
+    
+    /*Este método muestra los tipos de recorrido posibles del arbol, EL IT
+    DE LA LISTA ENLAZADA SE COMPARA CON "i" PARA PODER IMPRIMIR EN EL TEXT
+    ÁREA EL RECORRIDO DEL ARBOL BINARIO SEGÚN LOS NODOS INSERTADOS POR EL 
+    TEXTFIELD*/
+    
+    private String recorrido(LinkedList it, String msg) {
+        int i = 0;
+        String r = msg + "\n";
+        while (i < it.size()) {
+            r += "\t" + it.get(i).toString() + "";
+            i++;
+        }
+        return (r);
+    }
+}
